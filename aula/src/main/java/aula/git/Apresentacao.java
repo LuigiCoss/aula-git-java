fcbcbd2bf5caac3a750e564810ea9adc9582a200
LuigiCoss <luigipalmeiracosta@gmail.com>
2024-03-26 10:22:39 -0300

    mudei a função lucas para luigi

--- aula/src/main/java/aula/git/Apresentacao.java
+++ aula/src/main/java/aula/git/Apresentacao.java
@@ -4,10 +4,17 @@ public class Apresentacao {
 
     public void apresentar() {
         this.lucasSouza();
+        this.luigi();
     }
 
     private void lucasSouza() {
         System.out.println("");
+        System.out.println("Estou colaborando com o git");
+    }
+
+    
+    private void luigi() {
+        System.out.println("Poogers");
         System.out.println("Estou colaborando com o git");
     }
 }
